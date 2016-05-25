6cd71b78fafc46370ec1c5ab835eb9daa636159d
Kevin Clark <Kevin Clark>
2016-05-24 23:11:31 -0600

    add bubble listener to draw bubbles on mouse press

--- src/BubblePanel.java
+++ src/BubblePanel.java
@@ -1,14 +1,18 @@
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 public class BubblePanel extends JPanel {
 
     private ArrayList<Bubble> bubbleList;
+    private int size = 40;
 
     public BubblePanel() { //Constructor
 
         bubbleList = new ArrayList<Bubble>(); //Set up array list
+
+        addMouseListener(new BubbleListener());
 
         setBackground(Color.BLACK); //Set background for panel to black
         setPreferredSize(new Dimension(600,400)); //Set size
@@ -18,10 +22,43 @@ public class BubblePanel extends JPanel {
         super.paintComponent(page);
 
         //draw all the bubbles from bubblelist
+        for (Bubble bubble:bubbleList) {
+            page.fillOval(bubble.x, bubble.y, bubble.size, bubble.size);
+        }
         //Write the number of bubbles on the screen
 
         page.setColor(Color.GREEN);
         page.drawString("Count: " + bubbleList.size(), 5, 15);
+    }
+
+    private class BubbleListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            //Add to the bubblelist my mouse location and repaint
+            bubbleList.add(new Bubble(e.getX(), e.getY(), size));
+            repaint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 
 
