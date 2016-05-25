a0e30647e07869404a39d82d0bb417c12415ca19
Kevin Clark <Kevin Clark>
2016-05-24 22:39:32 -0600

    Add jpanel with Bubble class

--- src/BubblePanel.java
+++ src/BubblePanel.java
@@ -1,5 +1,31 @@
 import javax.swing.JPanel;
+import java.util.ArrayList;
 
 public class BubblePanel extends JPanel {
 
+    private ArrayList<Bubble> bubbleList;
+
+    public BubblePanel() { //Constructor
+
+        bubbleList = new ArrayList<Bubble>(); //Set up array list
+
+        setBackground(Color.BLACK); //Set background for panel to black
+        setPreferredSize(new Dimension(600,400)); //Set size
+    }
+
+
+
+
+    private class Bubble {
+        /**A Bubble needs an x, y location and a size */
+        public int x;
+        public int y;
+        public int size;
+
+        public Bubble(int newX, int newY, int newSize) { //Constructor
+            x = newX;
+            y = newY;
+            size = newSize;
+        }
+    }
 }
